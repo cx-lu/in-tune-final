e8f75e60e8999868b7dbc52b77ea78fe75c1619f
cx-lu <christina.lu.12@gmail.com>
2019-11-21 13:46:17 -0500

    Project initialization

---
+++ InTune/Entry.swift
@@ -0,0 +1,13 @@
+//
+//  Entry.swift
+//  InTune
+//
+//  Created by Christina Lu on 11/21/19.
+//  Copyright © 2019 Christina Lu. All rights reserved.
+//
+
+import Foundation
+
+class Entry {
+    
+}
